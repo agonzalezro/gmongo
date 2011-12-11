d94ec4e9bff127dd266605dad699cfe763d2e0c5
Álex González <agonzalezro@gmail.com>
2011-12-11 01:59:33 +0100

    added README link

--- mongo.py
+++ mongo.py
@@ -12,15 +12,31 @@ class Mongo():
     def __init__(self):
         self.connection = pymongo.Connection()
 
+    def _get_database(self, database):
+        return getattr(self.connection, database)
+
+    def _get_collection(self, database, collection):
+        return getattr(self._get_database(database), collection)
+
     def get_all_databases(self):
         return self.connection.database_names()
 
-    def get_all_documents(self, database):
+    def get_all_collections(self, database):
         database = getattr(self.connection, database)
         return database.collection_names()
 
-    def get_count(self, database, document=None):
+    def get_all_documents(self, database, collection):
         database = getattr(self.connection, database)
-        if document:
-            return getattr(database, document).count()
+        collection = getattr(database, collection)
+        return collection.find()
+
+    def get_content(self, database, collection, filter):
+        collection = self._get_collection(database, collection)
+        key = collection.find()[0].keys()[0]
+        return collection.find({key: filter})[0]
+
+    def get_count(self, database, collection=None):
+        database = getattr(self.connection, database)
+        if collection:
+            return getattr(database, collection).count()
         return len(database.collection_names())
--- views.py
+++ views.py
@@ -1,26 +1,120 @@
+import json
+
 from gi.repository import Gtk, GdkPixbuf
+
+from mongo import Mongo
+
+
+def get_icon(type):
+    #FIXME: Gtk.IconSize.MENU
+    icons = {"db": "folder-new",
+             "doc": "document",
+             "col": "document"}
+    img = Gtk.IconTheme.get_default().load_icon(icons[type], 32, 0)
+    return img
+
 
 class MainWindow(Gtk.Window):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.connect_signals()
-        self.create_grid()
-        self.dbs = DatabasesView()
-        self.hbox.pack_end(self.dbs.scroll, True, True, 0)
+        paned = self.get_paned()
+        self.documents = DocumentsView(self)
+        self.dbs = DatabasesView(self.documents)
+        self.vbox = Gtk.VBox()
+        self.toolbar = self.get_toolbar()
+        self.vbox.pack_start(self.toolbar, False, True, 0)
+        self.vbox.pack_end(self.documents.scroll, True, True, 0)
+        paned.pack1(self.vbox, True)
+        paned.pack2(self.dbs.scroll, False)
         self.resize(480, 320)
 
-    def create_grid(self):
-        self.hbox = Gtk.HBox()
-        self.add(self.hbox)
+    def get_paned(self):
+        paned = Gtk.Paned()
+        self.add(paned)
+        return paned
+
+    def get_toolbar(self):
+        toolbar = Gtk.Toolbar()
+        self.back = Gtk.ToolButton.new_from_stock(Gtk.STOCK_GO_BACK)
+        self.back.show()
+        toolbar.insert(self.back, -1)
+        toolbar.hide()
+        toolbar.set_property('no-show-all', True)
+        return toolbar
 
     def connect_signals(self):
         self.connect('delete-event', Gtk.main_quit)
 
 
+class DocumentView(object):
+    def __init__(self, window, database, collection, filter):
+        self.buffer = Gtk.TextBuffer()
+        text = Gtk.TextView()
+        text.set_buffer(self.buffer)
+        text.show_all()
+        # Replace the IconView with the TextView
+        window.vbox.get_children()[1].hide()
+        window.vbox.pack_end(text, True, True, 0)
+        # Show the toolbar to go back
+        window.toolbar.show()
+        self.fill_document(database, collection, filter)
+
+    def fill_document(self, database, collection, filter):
+        mongo = Mongo()
+        text = self.parse(mongo.get_content(database, collection, filter))
+        self.buffer.set_text(text)
+
+    def parse(self, content):
+        return json.dumps(content, indent=2)
+
+
+class DocumentsView(object):
+    def __init__(self, window):
+        self.store = Gtk.ListStore(GdkPixbuf.Pixbuf, str)
+        self.list = self.get_list()
+        self.scroll = self.get_scroll(self.list)
+        self.window = window
+        self.connect_signals()
+
+    def connect_signals(self):
+        self.list.connect('item-activated', self.on_item_activated)
+
+    def get_list(self):
+        list = Gtk.IconView()
+        list.set_model(self.store)
+        list.set_pixbuf_column(0)
+        list.set_text_column(1)
+        return list
+
+    def get_scroll(self, list):
+        scroll = Gtk.ScrolledWindow()
+        scroll.add(list)
+        return scroll
+
+    def on_item_activated(self, widget, item):
+        DocumentView(self.window,
+                     self.database,
+                     self.collection,
+                     self.list.get_model()[item][1])
+
+    def fill_documents(self, database, collection):
+        self.database = database
+        self.collection = collection
+        mongo = Mongo()
+        self.store.clear()
+        for d in mongo.get_all_documents(database, collection):
+            title = str(d.get(d.keys()[0]))
+            self.store.append([get_icon('doc'), title])
+
+
 class DatabasesView(object):
-    def __init__(self):
-        self.store = Gtk.TreeStore(str, GdkPixbuf.Pixbuf, int, bool)
-        self.scroll = self.get_scroll(self.get_tree())
+    def __init__(self, documents):
+        self.store = Gtk.TreeStore(GdkPixbuf.Pixbuf, str, int, bool)
+        self.tree = self.get_tree()
+        self.scroll = self.get_scroll(self.tree)
+        self.connect_signals()
+        self.documents = documents
 
     def get_tree(self):
         '''Create the two columns to show the dbs/collections and the counter.
@@ -32,8 +126,8 @@ class DatabasesView(object):
 
         collections_column.pack_start(cell_img, False)
         collections_column.pack_start(cell_text, True)
-        collections_column.add_attribute(cell_text, "text", 0)
-        collections_column.add_attribute(cell_img, "pixbuf", 1)
+        collections_column.add_attribute(cell_img, "pixbuf", 0)
+        collections_column.add_attribute(cell_text, "text", 1)
 
         counts_column = Gtk.TreeViewColumn("Count")
         counts_column.pack_start(cell_text, True)
@@ -51,48 +145,28 @@ class DatabasesView(object):
         return scroll
 
     def add_row(self, parent, image, name, count, is_db):
-        return self.store.append(parent, [name, image, count, is_db])
-
-
-'''class DatabasesView(Gtk.IconView):
-    def __init__(self):
-        super(DatabasesView, self).__init__()
-        self.create_model()
-        self.connect_signals()
-        self.mongo = Mongo()
-
-    def create_model(self):
-        model = Gtk.ListStore(str, GdkPixbuf.Pixbuf)
-        self.set_model(model)
-        self.set_text_column(0)
-        self.set_pixbuf_column(1)
+        return self.store.append(parent, [image, name, count, is_db])
 
     def connect_signals(self):
-        self.connect('item-activated', self.on_item_activated)
+        self.tree.connect('row-activated', self.on_row_activated)
+
+    def on_row_activated(self, widget, path, view_column):
+        model = widget.get_model()
+        if not model[path][3]:
+            self.documents.fill_documents(model[path].get_parent()[1],
+                                          model[path][1])
 
     def fill_databases(self):
-        for database in self.mongo.get_all_databases():
-            self.add_item(database)
-
-    def add_item(self, text):
-        pixbuf = Gtk.IconTheme.get_default().load_icon('gtk-floppy', 16, 0)
-        self.get_model().append([text, pixbuf])
-
-    def on_item_activated(self, widget, item):
-        self = DatabaseView(self.get_model()[item][0])
-        self.fill_collections()
-
-
-class DatabaseView(DatabasesView):
-    def __init__(self, database):
-        super(DatabaseView, self).__init__()
-        self.database = database
-
-    def fill_collections(self):
-        import ipdb;ipdb.set_trace()
-        for document in self.mongo.get_all_documents(self.database):
-            self.add_item(document)
-
-    def add_item(self, text):
-        pixbuf = Gtk.IconTheme.get_default().load_icon('gtk-add', 16, 0)
-        self.get_model().append([text, pixbuf])'''
+        mongo = Mongo()
+        for database in mongo.get_all_databases():
+            row = self.add_row(None,
+                               get_icon('db'),
+                               database,
+                               mongo.get_count(database),
+                               True)
+            for document in mongo.get_all_collections(database):
+                self.add_row(row,
+                             get_icon('doc'),
+                             document,
+                             mongo.get_count(database, document),
+                             False)
